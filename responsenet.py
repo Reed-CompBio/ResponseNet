decce488088e771f75909cbff9f81f7eb17a0ba6
gabeah <gabeh@reed.edu>
2024-07-18 00:38:13 -0400

    ResponseNet is operational off of command-line, theoretically ready to begin wrapping w/ spras

--- responsenet.py
+++ responsenet.py
@@ -6,7 +6,16 @@ import networkx as nx
 import math
 
 def parse_nodes(node_file):
-    ''' Parse a list of sources or targets and return a set '''
+    """ 
+    Parse a list of sources or targets and return a set 
+    
+    Parameters:
+        @node_file : PATH()
+            the PATH file for a list of nodes
+
+    Returns:
+        @nodes: set of all nodes listed in file
+    """
     with open(node_file) as node_f:
         lines = node_f.readlines()
         nodes = set(map(str.strip, lines))
@@ -18,7 +27,7 @@ def construct_digraph(edges_file, default_capacity= 1):
     returns a graph object and idDict
     
     Parameters:
-        edges_file : PATH()
+        @edges_file : PATH()
             the PATH file for an interactome
     
     Returns:
@@ -59,8 +68,24 @@ def construct_digraph(edges_file, default_capacity= 1):
         idDict["maxID"] = curID
         return G, idDict
     
-def add_sources_and_targets(G, sources, targets, idDict, flow):
+def add_sources_and_targets(G, sources, targets, idDict):
     """
+    Add a false source and target node to the DiGraph, helpful
+    for organization and essential to the ILP
+
+    Parameters:
+        @G : nx.DiGraph()
+            DiGraph object
+        @sources : set()
+            set of all source nodes
+        @targets : set()
+            set of all target nodes
+        @idDict : dict()
+            dictionary that assigns integer values to each node
+            to uniquely identify each node
+
+    Returns:
+        @G : modified DiGraph object with faux source and target
     """
     source_weight = 1/len(sources)
     target_weight = 1/len(targets)
@@ -101,6 +126,21 @@ def add_sources_and_targets(G, sources, targets, idDict, flow):
     return G
     
 def prepare_variables(solver, G, default_capacity= 1):
+    """
+    This section systematically creates variables for the ILP and saves them
+    both in a dictionary and as an attribute for each edge in G
+
+    Parameters:
+        @solver : pywraplp.Solver()
+            solver object that the LP depends on
+        @G : nx.DiGraph()
+            graph object of interactome
+        @default_capacity : int()
+            capacity value, defaulted to 1. Can be overwritten.
+
+    Returns:
+        @flows: dictionary of all variables in the solver
+    """
     flows = dict()
     extras = 0
     for edge in G.edges():
@@ -112,14 +152,30 @@ def prepare_variables(solver, G, default_capacity= 1):
             print(edge)
             extras += 1
     print(f"We had {extras} repeat edges")
-    
-    print('**'*25)
-    print(solver.ExportModelAsLpFormat(False).replace('\\', '').replace(',_', ','), sep='\n')
-    print('**'*25)
+
+    # Helpful debugging statement for LP solver    
+    # print('**'*25)
+    # print(solver.ExportModelAsLpFormat(False).replace('\\', '').replace(',_', ','), sep='\n')
+    # print('**'*25)
     
     return flows
     
 def prepare_constraints(solver, G, idDict):
+    """
+    This section systematically applies constraints on each node and all edges
+    to make sure that any flow entering a node also exits a node
+
+    Parameters:
+        @solver : pywraplp.Solver()
+            solver object that LP depends on
+        @G : nx.DiGraph()
+            graph object of interactome
+        @idDict : dict()
+            dictionary of all nodes in network
+
+    Returns:
+        @constraints: list object containing all constraints in the LP
+    """
     constraints = []
     for i,  node in enumerate(G.nodes):
         
@@ -150,14 +206,32 @@ def prepare_constraints(solver, G, idDict):
         
     constraints[-1].SetBounds(0,0)
 
-    
-    print('**'*25)
-    print(solver.ExportModelAsLpFormat(False).replace('\\', '').replace(',_', ','), sep='\n')
-    print('**'*25)
+    # Helpful debugging statement for LP    
+    # print('**'*25)
+    # print(solver.ExportModelAsLpFormat(False).replace('\\', '').replace(',_', ','), sep='\n')
+    # print('**'*25)
     
     return constraints
             
 def prepare_objective(solver, G, flows, gamma, s):
+    """
+    This segment goes through all edges in the graph and sets a coefficient on each variable in the LP
+
+    Parameters:
+        @solver : pywraplp.Solver()
+            solver object that LP depends on
+        @G : nx.DiGraph()
+            graph object of interactome
+        @flows : dict()
+            dictionary of all flow variables for the solver
+        @gamma : int()
+            user defined value that determines graph size
+        @s : int()
+            integer stand-in for the "source" node
+    
+    Returns:
+        @objective : solver objective with all constraints
+    """
     objective = solver.Objective()
     
     for i,j in G.edges():
@@ -173,12 +247,27 @@ def prepare_objective(solver, G, flows, gamma, s):
     
     return objective  
     
-    print('**'*25)
-    print(solver.ExportModelAsLpFormat(False).replace('\\', '').replace(',_', ','), sep='\n')
-    print('**'*25)
+    # Helpful debugging statement to show status of LP solver
+    # print('**'*25)
+    # print(solver.ExportModelAsLpFormat(False).replace('\\', '').replace(',_', ','), sep='\n')
+    # print('**'*25)
 
 def responsenet(G, idDict, gamma, out_file):
-    """ The NEW ILP solver for ResponseNet, using glop.
+    """ 
+    The NEW ILP solver for ResponseNet, using GLOP.
+
+    Parameters:
+        @G : nx.DiGraph()
+            graph object of interactome
+        @idDict : dict()
+            dictionary of all nodes in network
+        @gamma : int()
+            user defined integer determining size of output graph
+        @out_file : PATH()
+            PATH to the output file for writing the LP solution
+    
+    Returns:
+        Nothing
     """
     
     solver = pywraplp.Solver.CreateSolver("GLOP")
@@ -202,15 +291,18 @@ def responsenet(G, idDict, gamma, out_file):
         print("The problem does not have an optimal solution.")
         return
     
-    write_output_to_sif(status, G, solver, out_file)
+    write_output_to_tsv(status, G, solver, out_file)
     
-def write_output_to_sif(status, G, solver, out_file, include_st = False):
+def write_output_to_tsv(status, G, solver, out_file, include_st = False):
     '''
     Write output of solver.Solve() over graph obj to an output file specified 
     by out_file
     
     Params:
         @status: status of solver.Solve()
+
+    # TODO: add check for if include_st == True/False
+    # TODO: Confirm if SIF is preferred output type
     '''
     with open(out_file, "w") as output_f:
         print(f"Objective value = {solver.Objective().Value():0.1f}")
@@ -218,24 +310,49 @@ def write_output_to_sif(status, G, solver, out_file, include_st = False):
         for u,v in G.edges:    
             if G[u][v]["flow"].solution_value() > 0.0:   
                 print(G[u][v]["flow"],'-->',G[u][v]["flow"].solution_value())
-                output_f.write(u+"\t"+v+"\t"+G[u][v]["flow"].solution_value())
-    
+                output_f.write(str(u)+"\t"+str(v)+"\t"+str(G[u][v]["flow"].solution_value()))
     return
 
 def main(args):
     
-    sources = parse_nodes("sources.txt")
-    targets = parse_nodes("targets.txt")
+    sources = parse_nodes(args.sources_file)
+    targets = parse_nodes(args.targets_file)
     
     gamma = args.gamma
     
-    G, idDict = construct_digraph("edges.txt")
+    G, idDict = construct_digraph(args.edges_file)
     
-    G = add_sources_and_targets(G, sources, targets, idDict, 1)
+    G = add_sources_and_targets(G, sources, targets, idDict)
     
-    responsenet(G, idDict, gamma)
+    out_file = args.output+"_gamma"+str(gamma)+".tsv"
+
+    responsenet(G, idDict, gamma, out_file)
     
-    
-    
-    
-    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--edges_file',
+                        help='Network file. File should be in SIF file format.',
+                        type=str,
+                        required=True)
+    parser.add_argument('--sources_file',
+                        help='File which denotes source nodes, with one node per line.',
+                        type=str,
+                        required=True)
+    parser.add_argument('--targets_file',
+                        help='File which denotes source nodes, with one node per line.',
+                        type=str,
+                        required=True)
+    parser.add_argument('--output',
+                        help='Prefix for all output files.',
+                        type=str,
+                        required=True)
+    parser.add_argument('--gamma',
+                        help='The size of the output graph',
+                        type=int,
+                        required=True,
+                        default=10)
+
+    args = parser.parse_args()
+main(args)
